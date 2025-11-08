b92ab0643d890146775d3fc275343d1864209a2b
MRG-G <117476398+MRG-G@users.noreply.github.com>
2025-11-08 17:05:16 +0400

    Delete ethereumbot directory

--- ethereumbot/utils/keyboards.py
+++
@@ -1,8 +0,0 @@
-# utils/keyboards.py
-from telegram import ReplyKeyboardMarkup
-
-def build_menu_kb(texts_lang: dict) -> ReplyKeyboardMarkup:
-    return ReplyKeyboardMarkup(texts_lang["buttons"], resize_keyboard=True)
-
-def build_lang_kb(texts_lang: dict) -> ReplyKeyboardMarkup:
-    return ReplyKeyboardMarkup(texts_lang["lang_keyboard"], resize_keyboard=True, one_time_keyboard=True)
